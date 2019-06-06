08d881416068d517cbb1e40f87fe4e2cf151d688
Kira Corbin <madkatkeys@gmail.com>
2019-06-06 09:34:08 -0700

    uploaded with changes that fix a score error

--- assets/js/crystal.js
+++ assets/js/crystal.js
@@ -1,6 +1,60 @@
 var win = 0;
 var lose = 0;
-var score =0;
+var score = 0;
+var targetScore = 0;
+
+function getRandom(min, max){
+    var randomNum = Math.floor(Math.random() * max) + min;
+    return randomNum
+}
+
+var randomCrystalNum = getRandom(1, 10)
+// console.log(randomCrystalNum)
+
+var crystal = {
+    blue: {
+        name: 'Blue',
+        value: 0,
+    },
+    pink: {
+        name: 'Pink',
+        value: 0
+    },
+    purple: {
+        name: 'Purple',
+        value: 0
+    },
+    red: {
+        name: 'Red',
+        value: 0
+    }
+}
+
+crystal.blue = getRandom(1, 12)
+crystal.pink = getRandom(1, 12)
+crystal.purple = getRandom(1, 12)
+crystal.red = getRandom(1, 12)
+targetScore = getRandom(1,12)
+// console.log(crystal.red)
+// console.log(crystal.pink)
+// console.log(crystal.purple)
+// console.log(crystal.blue)
+// console.log(targetScore)
+
+$("#blue").click(function(){
+    // console.log("blue's been clicked");
+});
+$("#pink").click(function(){
+    // console.log("pink's been clicked");
+});
+$("#purple").click(function(){
+    // console.log("purple's been clicked");
+});
+$("#red").click(function(){
+    // console.log("red's been clicked");
+});
+
+
 
 
 // psydocode 
@@ -12,16 +66,19 @@ var score =0;
 
 
 // random number score
-var random_number = Math.floor(Math.random() * 19) + 120;
 
 // random number crystal code
-var random_crystal = Math.floor(Math.random() * 11) + 1;
 
 
-var purplecrystal = random_crystal(1, 12);
-var pinkcrystal = random_crystal(1, 12);
-var bluecrystal = random_crystal(1, 12);
-var redcrystal = random_crystal(1, 12);
+
+// var purplecrystal = random_crystal(1, 12);
+// var pinkcrystal = random_crystal(1, 12);
+// var bluecrystal = random_crystal(1, 12);
+// var redcrystal = random_crystal(1, 12);
+
+
+
+
 
 
 
@@ -34,62 +91,25 @@ var redcrystal = random_crystal(1, 12);
 // example $("#wins").html(wins)
 
 
-
-
 // for (var i = 0; i < 4; i++) {
 
 //     var crystal = $("<div>");
-//     crystal.attr({
-//         "class": 'crystal',
-//         "data-random": random
-//     });
-//     crystal.css({
-//         "background-image": "url('" + images[i] + "')",
-//         "background-size": "cover"
-//     });
-
-//     $(".crystals").append(crystal);
-// }
-// $("#previous").html("Total Score: " + previous);
 // }
 
 
-// resetAndStart();
 
-
-// // Event Delegation
-// $(document).on('click', ".crystal", function () {
-
-//     var num = parseInt($(this).attr('data-random'));
-
-//     previous += num;
-
-
-//     $("#previous").html("Total score: " + previous);
 
 //     console.log(previous);
 
-//     if (previous > random_result) {
+//         losses++;
 
-//         lost++;
-
-//         $("#lost").html("You lost: " + lost);
-
-//         previous = 0;
-
-//         resetAndStart();
+//         $("#losses").html("You losses: " + losses);
 
 //     }
 //     else if (previous === random_result) {
-
 //         win++;
-
-//         $("#win").html("You win: " + win);
-
-//         previous = 0;
-
-//         resetAndStart();
-
+//         $("#wins").html("You wins: " + win);
+//       
 //     }
 
 // });
